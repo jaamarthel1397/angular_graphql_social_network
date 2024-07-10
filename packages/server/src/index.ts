019711cf74e83820f18e1992872a557023745dfb
jaamarthel1397 <jaamarthel@gmail.com>
2024-07-10 11:56:57 -0600

    added typeorm connection to mysql

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -5,17 +5,24 @@ import express from 'express';
 import http from 'http';
 import cors from 'cors';
 import schema from './graphql/schema';
-import { addMocksToSchema } from '@graphql-tools/mock';
-import mocks from './graphql/mocks';
+import AppDataSource from './ormconfig';
 
 interface MyContext {
   token?: String;
 }
 
+AppDataSource.initialize()
+  .then(() => {
+    StartServer();
+  })
+  .catch(error => {
+    console.log("Database connection error: ", error);
+  })
+
 async function StartServer() {
     const app = express();
     const httpServer = http.createServer(app);
-    const server = new ApolloServer<MyContext>({ schema: addMocksToSchema({schema, mocks})});
+    const server = new ApolloServer<MyContext>({ schema });
     await server.start();
     app.use(
         '/graphql',
@@ -28,5 +35,3 @@ async function StartServer() {
     await new Promise<void>((resolve) => httpServer.listen({ port: 8080 }, resolve));
     console.log(`🚀 Server ready at http://localhost:8080/graphql`);
 }
-
-StartServer();
---
+++ packages/server/src/ormconfig.ts
@@ -0,0 +1,14 @@
+import { DataSource } from 'typeorm';
+
+const AppDataSource = new DataSource({
+  type: "mysql",
+  host: "localhost",
+  port: 3306,
+  username: "dbuser",
+  password: "password",
+  database: "socialdb",
+  synchronize: true,
+  logging: false
+});
+
+export default AppDataSource;
